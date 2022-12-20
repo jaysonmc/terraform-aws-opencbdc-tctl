6ee98a3519018754c21f8d53870a1b88fbce699f
Jayson Sizer McIntosh <jaysosmc@amazon.com>
2022-12-20 09:22:00 -0500

    adding tags to cf vars file

--- modules/cloudfront/variables.tf
+++ modules/cloudfront/variables.tf
@@ -2,3 +2,9 @@ variable "s3_build_bucket" {
   type        = string
   description = "S3 bucket where the react build folder exists."
 }
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to set for all resources"
+  default     = {}
+}
