d576d32a6a1640c08a73bdd20de12159001ca0d8
Jayson Sizer McIntosh <jaysosmc@amazon.com>
2022-12-20 13:20:42 -0500

    passing in the proper cloudfront address (rather than hard coding the previous distribution)

--- modules/test-controller/variables.tf
+++ modules/test-controller/variables.tf
@@ -154,3 +154,9 @@ variable "certbot_lambda_build_in_docker" {
   description = "Determines whether or not to build certbot lambda function in docker."
   default     = true
 }
+
+variable "cloudfront_address" {
+  type = string
+  description = "Domain of CloudFront distribution"
+  default = ""
+}
