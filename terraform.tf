fb2e7909b9b951f5ffdedd97357378d63a7acadd
Jayson Sizer McIntosh <jaysosmc@amazon.com>
2022-12-30 13:08:26 -0500

    configuring backend to use s3

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,10 @@
 terraform {
-  required_version = "= 1.2.1"
-  backend "s3" {}
+  required_version = "= 1.2.8"
+  backend "s3" {
+    bucket = "jaysosmc-opencbdc-artifacts"
+    key    = "test.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
