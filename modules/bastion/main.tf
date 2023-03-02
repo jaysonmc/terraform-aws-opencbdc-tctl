9b9fdf7e6d9d121c3721c642967b89415b2c5aff
Jayson Sizer McIntosh <jayson.r.mcintosh@gmail.com>
2023-03-02 13:52:54 -0500

    Update main.tf
    Signed-off-by: Jayson Sizer McIntosh <jayson.r.mcintosh@gmail.com>

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -65,7 +65,7 @@ data "aws_ami" "bastion" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server"]
   }
 }
 
